86d856a5ebd3256549dd1da72eaa7e964335b6ba
Nico Vermaas <vermaas@astron.nl>
2022-04-24 16:26:40 +0200

    using environment vars

--- homebase/homebase/settings/base.py
+++ homebase/homebase/settings/base.py
@@ -9,10 +9,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!sls2(u-2^+*(ob%hc5xrifx0i#ekgguj%c3i_k@ia001w$it0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
--- homebase/homebase/settings/dev.py
+++ homebase/homebase/settings/dev.py
@@ -6,6 +6,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
+SECRET_KEY = "very_secret"
 
 ALLOWED_HOSTS = [ '*' ]
 CORS_ORIGIN_ALLOW_ALL = True
